bfb68a7d6a7241aa2aaf46c166976ddaf879865b
Trujillo Alessandro <alessandrott2004@gmail.com>
2026-03-16 13:40:15 -0500

    parte b se modifico el archivo, añadiendo una función async

--- nivel_4_arrays/operaciones.js
+++ nivel_4_arrays/operaciones.js
@@ -59,3 +59,13 @@ export function simularRespuestaServidor(resultado) {
     }, 2000);
   });
 }
+export async function venderPlatoAsync(nombre, cantidad) {
+  const resultado = venderPlato(nombre, cantidad);
+
+  if (!resultado.ok) {
+    throw new Error(resultado.mensaje);
+  }
+
+  const respuesta = await simularRespuestaServidor(resultado.mensaje);
+  return respuesta;
+}
